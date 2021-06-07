ed5cd4743d4c9fc4b9f2cb33434da6621e85373f
Murial <mukmin19ti@mahasiswa.pcr.ac.id>
2021-06-07 16:22:56 +0700

    Blocking UI for all pages

---
+++ slide_puzzle/in_game.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace slide_puzzle
+{
+    /// <summary>
+    /// Interaction logic for in_game.xaml
+    /// </summary>
+    public partial class in_game : Window
+    {
+        public in_game()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ slide_puzzle/main_menu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace slide_puzzle
+{
+    /// <summary>
+    /// Interaction logic for main_menu.xaml
+    /// </summary>
+    public partial class main_menu : Window
+    {
+        public main_menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
